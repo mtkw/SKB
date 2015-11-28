1292dcf0b1f7a7b9a2f7cad6f34405718ba46272
Mati <mati.kaweczynski@gmail.com>
2015-11-28 22:16:17 +0100

    Kolejne uzupełnienie schematu aplikacji w dodtkowe klasy w module ejb.
    Wykryłem problem z regułami nawigacji w naszym menu niestety nie są one rozponawane przez elementy menu i narazie nie wiem jak to obejść w internecie znalazłem ten bład ale nie jego rozwiązanie...

---
+++ SKB_Ubezpieczenia/SKB_Ubezpieczenia-ejb/src/main/java/ubezpieczenia/insurance/InsuranceEndPoint.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ubezpieczenia.insurance;
+
+import javax.ejb.Stateless;
+
+/**
+ *
+ * @author Mateusz
+ */
+@Stateless
+public class InsuranceEndPoint implements InsuranceEndPointLocal {
+
+    // Add business logic below. (Right-click in editor and choose
+    // "Insert Code > Add Business Method")
+}
--- SKB_Ubezpieczenia/SKB_Ubezpieczenia-web/src/main/java/ubezpieczenia/primefaces/MenuView.java
+++ SKB_Ubezpieczenia/SKB_Ubezpieczenia-web/src/main/java/ubezpieczenia/primefaces/MenuView.java
@@ -8,6 +8,8 @@ import org.primefaces.model.menu.DefaultMenuItem;
 import org.primefaces.model.menu.DefaultMenuModel;
 import org.primefaces.model.menu.DefaultSubMenu;
 import org.primefaces.model.menu.MenuModel;
+
+//Nie używana klasa jak sie okazało
  
 @ManagedBean
 public class MenuView {
@@ -16,39 +18,39 @@ public class MenuView {
  
     @PostConstruct
     public void init() {
-        model = new DefaultMenuModel();
+       // model = new DefaultMenuModel();
          
         //First submenu
-        DefaultSubMenu firstSubmenu = new DefaultSubMenu("Dynamic Submenu");
-         
-        DefaultMenuItem item = new DefaultMenuItem("External");
-        item.setUrl("http://www.primefaces.org");
-        item.setIcon("ui-icon-home");
-        firstSubmenu.addElement(item);
-         
-        model.addElement(firstSubmenu);
+//        DefaultSubMenu firstSubmenu = new DefaultSubMenu("Dynamic Submenu");
+//         
+//        DefaultMenuItem item = new DefaultMenuItem("External");
+//        item.setUrl("http://www.primefaces.org");
+//        item.setIcon("ui-icon-home");
+//        firstSubmenu.addElement(item);
+//         
+//        model.addElement(firstSubmenu);
          
         //Second submenu
-        DefaultSubMenu secondSubmenu = new DefaultSubMenu("Dynamic Actions");
- 
-        item = new DefaultMenuItem("Save");
-        item.setIcon("ui-icon-disk");
-        item.setCommand("#{menuView.save}");
-        item.setUpdate("messages");
-        secondSubmenu.addElement(item);
-         
-        item = new DefaultMenuItem("Delete");
-        item.setIcon("ui-icon-close");
-        item.setCommand("#{menuView.delete}");
-        item.setAjax(false);
-        secondSubmenu.addElement(item);
-         
-        item = new DefaultMenuItem("Redirect");
-        item.setIcon("ui-icon-search");
-        item.setCommand("#{menuView.redirect}");
-        secondSubmenu.addElement(item);
- 
-        model.addElement(secondSubmenu);
+//        DefaultSubMenu secondSubmenu = new DefaultSubMenu("Dynamic Actions");
+// 
+//        item = new DefaultMenuItem("Save");
+//        item.setIcon("ui-icon-disk");
+//        item.setCommand("#{menuView.save}");
+//        item.setUpdate("messages");
+//        secondSubmenu.addElement(item);
+//         
+//        item = new DefaultMenuItem("Delete");
+//        item.setIcon("ui-icon-close");
+//        item.setCommand("#{menuView.delete}");
+//        item.setAjax(false);
+//        secondSubmenu.addElement(item);
+//         
+//        item = new DefaultMenuItem("Redirect");
+//        item.setIcon("ui-icon-search");
+//        item.setCommand("#{menuView.redirect}");
+//        secondSubmenu.addElement(item);
+// 
+//        model.addElement(secondSubmenu);
     }
  
     public MenuModel getModel() {
