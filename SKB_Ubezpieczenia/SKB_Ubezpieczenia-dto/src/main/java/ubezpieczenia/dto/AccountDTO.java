c5a84b767c3d504628209fb855723de065d91900
Mati <mati.kaweczynski@gmail.com>
2015-12-02 17:06:36 +0100

    Ogólna Poprawa wszystkich dotychczasowych błedów. Projekt tworzy się poprawnie baza danych prawidłowo działa wszystko sie tworzy tak jak powinno

--- SKB_Ubezpieczenia/SKB_Ubezpieczenia-dto/src/main/java/ubezpieczenia/dto/AccountDTO.java
+++ SKB_Ubezpieczenia/SKB_Ubezpieczenia-dto/src/main/java/ubezpieczenia/dto/AccountDTO.java
@@ -5,14 +5,79 @@
  */
 package ubezpieczenia.dto;
 
+import java.io.Serializable;
+import java.util.Objects;
+
 /**
  *
  * @author Mateusz
  */
-public class AccountDTO {
-    
+public class AccountDTO implements Serializable {
+
     private String login;
     private String password;
     private String mail;
     
+    //Nie jestem pewien czy to jest dobrze
+    private String customer;
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    public String getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(String customer) {
+        this.customer = customer;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 37 * hash + Objects.hashCode(this.login);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final AccountDTO other = (AccountDTO) obj;
+        if (!Objects.equals(this.login, other.login)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "AccountDTO{" + "login=" + login + ", password=" + password + ", mail=" + mail + ", customer=" + customer + '}';
+    }
+
 }
